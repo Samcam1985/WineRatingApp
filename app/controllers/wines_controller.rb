30ed410c6b2a8aed1a24d1018c74327dfebc9769
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-09-01 10:33:57 +0100

    database displaying on server

---
+++ app/controllers/wines_controller.rb
@@ -0,0 +1,8 @@
+class WinesController < ApplicationController
+
+  def index
+    wines = Wine.all
+    render :json => wines
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  #get request sent to /wines should go to wines_controller method
+  get 'wines' => 'wines#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
