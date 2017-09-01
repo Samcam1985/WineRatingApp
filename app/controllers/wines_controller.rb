77c5a245813a6d34da09d2284391a6d4b8979925
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-09-01 14:14:30 +0100

    can delete wine from database, tested in insomnia

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -14,11 +14,23 @@ class WinesController < ApplicationController
       image: params[:image]
     })
     render :json => wine
-    
+
   end
 
   def show
     wine = Wine.find( params[:id] )
     render :json => wine
+end
+
+def destroy
+  wine = Wine.find( params[:id] )
+
+
+  if wine.destroy!
+    render :json => {status: :Wine_Deleted}
+  else
+    render :json => {status: :delete_failed}
+
+end
 end
 end
