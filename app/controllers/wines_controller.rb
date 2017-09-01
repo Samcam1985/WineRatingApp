13231d17f2f262011953bad64f702737a5039ada
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-09-01 14:01:39 +0100

    can add wine to database, tested in insomnia

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -5,4 +5,20 @@ class WinesController < ApplicationController
     render :json => wines
   end
 
+  def create
+    wine = Wine.create( {
+      name: params[:name],
+      colour: params[:colour],
+      country: params[:country],
+      year: params[:year],
+      image: params[:image]
+    })
+    render :json => wine
+    
+  end
+
+  def show
+    wine = Wine.find( params[:id] )
+    render :json => wine
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   #get request sent to /wines should go to wines_controller method
-  get 'wines' => 'wines#index'
+  # get 'wines' => 'wines#index'
+  resources :wines
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
