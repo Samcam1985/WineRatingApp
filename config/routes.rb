5020cadb52bc9a665bc9ebd36f208e48f77ab868
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-09-01 15:21:23 +0100

    wine details rendered to screen using react

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   #get request sent to /wines should go to wines_controller method
   # get 'wines' => 'wines#index'
-  resources :wines
+
+  resources :wines, defaults: {format: :json}
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
